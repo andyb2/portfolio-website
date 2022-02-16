ef93663ce95cb971ac22dc7927f49f52e1f1ef29
AMB <a.michael.boyle@gmail.com>
2022-02-15 21:31:45 -0500

    base files created

---
+++ src/components/About/About.tsx
@@ -0,0 +1,11 @@
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="about" id="about">
+            <p className="about-text">Hello! My name is Andrew Boyle and am an aspiring software developer looking to accomplish my goals of becoming a software developer! Hello! My name is Andrew Boyle and am an aspiring software developer looking to accomplish my goals of becoming a software developer! Hello! My name is Andrew Boyle and am an aspiring software developer looking to accomplish my goals of becoming a software developer! Hello! My name is Andrew Boyle and am an aspiring software developer looking to accomplish my goals of becoming a software developer!</p>
+        </div>
+    )
+}
+
+export default About
