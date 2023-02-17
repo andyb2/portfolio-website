6d8eaf4f4eba8e959b43d373f44ebf5fdd8ffe1d
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-16 19:32:43 -0500

    prettier config changed formatting

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,4 @@
-import { createTheme } from "@mui/material/styles";
-import { lineHeight } from "@mui/system";
+import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   components: {
@@ -13,5 +12,5 @@ export const theme = createTheme({
         },
       },
     },
-  }
+  },
 });
