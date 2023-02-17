5f375b9af8c9e9be04ffbe2ddc4314dcabec7c06
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-16 19:31:12 -0500

    viewport height is now a hook

---
+++ src/hooks/ViewportHeight.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+interface Height {
+  height: number;
+  setHeight: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const ViewportHeight = ({ height, setHeight }: Height): any => {
+  useEffect(() => {
+    const getHeight = () => {
+      setHeight(window.innerHeight);
+      document.documentElement.style.setProperty(
+        '--doc-height',
+        `${window.innerHeight}px`
+      );
+    };
+    window.addEventListener('resize', getHeight);
+    return () => {
+      window.removeEventListener('resize', getHeight);
+    };
+  }, [height, setHeight]);
+};
+
+export default ViewportHeight;
