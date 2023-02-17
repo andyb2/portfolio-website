e088d4dd290690cea939a8ec62bd8eebe56a6f69
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-16 19:37:10 -0500

    when app is reloaded it will scroll to the top of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './theme';
-import Home from './components/Home';
-import Projects from './components/Projects';
-import ViewportHeight from './components/ViewportHeight';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import ViewportHeight from './hooks/ViewportHeight';
 
 function App() {
   const [height, setHeight] = useState<number>(window.innerHeight);
+
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual';
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
-          <ViewportHeight height={height} setHeight={setHeight}/>
-          <Home height={height} />
-          <Projects height={height} />
-    </ ThemeProvider>
+      <ViewportHeight height={height} setHeight={setHeight} />
+      <Home height={height} />
+      <Projects height={height} />
+    </ThemeProvider>
   );
 }
 
