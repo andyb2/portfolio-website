6763f41f5df17da2c57e432a32f31c7362ec3542
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-16 19:35:34 -0500

    Nav file reworked. css is now from emotion

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,47 +1,51 @@
-import { FC } from "react";
-import { Typography } from "@mui/material";
-import { Link } from "react-scroll";
+import { FC } from 'react';
+import { Link } from 'react-scroll';
+import styled from '@emotion/styled';
 
 interface IProps {
-  home?: boolean,
-  project?: boolean,
+  home?: boolean;
+  project?: boolean;
 }
+
+const NavContainer = styled.nav``;
+
+const NavUl = styled.ul`
+  list-style: none;
+  margin: 0;
+`;
+
+const NavItem = styled.li<any>`
+  position: relative;
+  z-index: 100;
+  font-size: 3rem;
+  color: ${({ project }) => (!project ? 'black' : '#c3ca86')};
+  margin: 0;
+  padding: 0;
+  padding-right: 1rem;
+  &:hover {
+    cursor: pointer;
+    color: ${({ project }) => (!project ? 'gray' : '#e0e5ab')};
+  }
+  @media (max-width: 700px) {
+    font-size: 2.5rem;
+  }
+`;
 
 const Nav: FC<IProps> = ({ home, project }) => {
-
-    return (
-      <>
-        { 
-          home &&
-            <Link to='projects' spy={true} smooth={true} duration={500}>
-              <Typography sx={{ 
-                position: 'relative',
-                            zIndex: '100',
-                            fontSize: '3rem',
-                            fontWeight: '300',
-                            margin: '0',
-                            padding: '0',
-                            '&:hover': { 
-                                cursor: 'pointer'
-                            },
-                            '@media (max-width: 700px)': {
-                              fontSize: '2.5rem',
-                            }
-                          }}
-              >
-                PROJECTS
-              </Typography>
-            </Link>  
-        }
-        {
-          project && 
-              <Link to="about" spy={true} smooth={true} duration={500}>
-                <Typography sx={{padding: 0, fontSize: '3rem', fontWeight: '300', color: 'black', '&:hover': { cursor: 'pointer' } }}>
-                  ABOUT
-                </Typography>
-              </Link>
-        }
-      </>
-    )
-}
-export default Nav
+  return (
+    <NavContainer>
+      <NavUl>
+        {home ? (
+          <Link to='projects' spy={true} smooth={true} duration={500}>
+            <NavItem project={home}>PROJECTS</NavItem>
+          </Link>
+        ) : (
+          <Link to='about' spy={true} smooth={true} duration={500}>
+            <NavItem home={project}>ABOUT</NavItem>
+          </Link>
+        )}
+      </NavUl>
+    </NavContainer>
+  );
+};
+export default Nav;
