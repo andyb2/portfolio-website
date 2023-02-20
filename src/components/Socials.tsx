24985addb3bd60df1287db4133e4ce13823fd60b
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-20 13:27:33 -0500

    no chnge

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,25 +1,33 @@
-import { Icon } from "@mui/material";
-import { Link } from "@mui/material";
+import { Icon } from '@mui/material';
+import { Link } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const style = {
-    color: '#c3ca86',
-    fontSize: '35px',
-    marginLeft: '0.1rem',
-}
+  color: '#c3ca86',
+  fontSize: '35px',
+  marginLeft: '0.1rem',
+};
 
 const Socials = () => {
-    return (
-        <>
-            <Link href="https://github.com/andyb2" target='_blank'>
-                <Icon sx={style} component={GitHubIcon}/>
-            </Link>
-            <Link href="https://www.linkedin.com/in/andrew-boyle-a9a596204/" target='_blank'>
-                <Icon sx={style} component={LinkedInIcon}/>
-            </Link>
-        </>
-    )
-}
+  return (
+    <>
+      <Link
+        href='https://github.com/andyb2'
+        aria-label='Github icon'
+        target='_blank'
+      >
+        <Icon sx={style} component={GitHubIcon} />
+      </Link>
+      <Link
+        href='https://www.linkedin.com/in/andrew-boyle-a9a596204/'
+        aria-label='LinkedIn Icon'
+        target='_blank'
+      >
+        <Icon sx={style} component={LinkedInIcon} />
+      </Link>
+    </>
+  );
+};
 
-export default Socials
+export default Socials;
