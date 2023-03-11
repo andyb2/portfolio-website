50c2346adac21a182d2e5f3279678114027287f6
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-10 20:50:59 -0500

    project links updated

--- src/projectList.ts
+++ src/projectList.ts
@@ -4,20 +4,20 @@ export const projectsList = [
     description:
       'Classic game where users choose tiles to connect three spaces in a row, column or diagonally. The user is playing against a bot that chooses their move depending on what the human player has selected',
     tech: ['NEXT.JS', 'FIREBASE', 'GRAPHQL', 'AIRBASE'],
-    link: 'https://andyb2.github.io/tictactoe/',
+    link: 'https://github.com/orrymevorach/assistant-captain',
   },
   {
     title: 'SPOTIFY USER - REPO',
     description:
       'Spotify users can log into the application and display their recently played songs, top songs, top artists, and playlists. ',
     tech: ['REACT', 'REDUX', 'EXPRESS', 'NODE', 'SPOTIFY API'],
-    link: 'Click here for the repository.',
+    link: 'https://github.com/andyb2/Spotify-User-Profile',
     image: './img/SpotifyProfile.JPG',
   },
   {
     title: 'OPEN CHAT - LIVE APP',
     description:
-      'A real-time chat application where users can join rooms and send messages within specific rooms. Private messages can be sent to users by clicking their names from the user list and typing into the chat input. Users are notified via a glowing red private message button in the sidebar. Data is not persistent between sessions.',
+      'A real-time chat application where users can join rooms and send messages to an entire room or send private messages to users.',
     tech: ['TYPESCRIPT', 'REACT', 'REDUX', 'SOCKET IO', 'NODE'],
     link: 'https://opchat.herokuapp.com/',
   },
