9d87193b857d3e1e875e23106eb4fa609f682189
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-10 19:06:47 -0500

    emotion styled now used for some components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
-import Socials from '../components/Socials';
 import About from '../components/About';
 import { Box } from '@mui/system';
-import Nav from '../components/Nav';
 import Intro from '../components/Intro';
 import styled from '@emotion/styled';
 
-export interface Height {
+export interface IProps {
   height: number;
+  active?: boolean;
+  setActive?: any;
 }
 
-const Andrew = styled.div<Height>`
-  color: #c3ca86;
+const Andrew = styled.div<IProps>`
   font-size: ${({ height }) => (height <= 450 ? '8vw' : '13vw')};
   font-weight: 300;
   margin: 0;
@@ -21,18 +19,16 @@ const Andrew = styled.div<Height>`
   padding-bottom: 0.5rem;
 `;
 
-const Software = styled.div<Height>`
+const Software = styled.div<IProps>`
   display: inline-block;
-  color: #c3ca86;
   font-size: ${({ height }) => (height <= 450 ? '4vw' : '6vw')};
   font-weight: 600;
   line-height: 0.65;
 `;
 
-const Developer = styled.div<Height>`
+const Developer = styled.div<IProps>`
   display: inline-block;
-  color: #c3ca86;
-  border: 1px solid #d4db96;
+  border: 1px solid black;
   font-size: ${({ height }) => (height <= 450 ? '3vw' : '5vw')};
   font-weight: 200;
   margin-left: 1;
@@ -40,51 +36,27 @@ const Developer = styled.div<Height>`
 `;
 
 const homeB1 = {
-  position: 'relative',
   display: 'flex',
   flexDirection: 'column',
-  height: '100vh',
-  background: 'black',
+  paddingTop: '55px',
 };
 
 const about = {
-  height: '100%',
-  width: '100%',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+  paddingBottom: '1rem',
 };
 
 const introduction = {
-  position: 'absolute',
-  height: '100%',
+  padding: '1rem',
   width: '100%',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
 };
 
-const homeB3 = {
-  position: 'absolute',
-  bottom: '0',
-  right: '0',
-};
-
-const socials = {
-  position: 'absolute',
-  zIndex: '100',
-  bottom: '0',
-  left: '0',
-  marginBottom: '0.4rem',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '0.1rem',
-};
-
-const Home = ({ height }: Height) => {
-  const [active, setActive] = useState(true);
-  const homePage = true;
-
+const Home = ({ height, active, setActive }: IProps) => {
   return (
     <Box id='about' sx={homeB1}>
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
@@ -94,19 +66,13 @@ const Home = ({ height }: Height) => {
         </Box>
         <Andrew height={height}>ANDREW BOYLE</Andrew>
       </Box>
-      <Box sx={about}>
-        {!active && <About active={active} height={height} />}
-      </Box>
       {active && (
         <Box sx={introduction}>
           <Intro active={active} setActive={setActive} />
         </Box>
       )}
-      <Box sx={homeB3}>
-        <Nav home={homePage} />
-      </Box>
-      <Box sx={socials}>
-        <Socials />
+      <Box sx={about}>
+        {!active && <About active={active} height={height} />}
       </Box>
     </Box>
   );
