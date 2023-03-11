d7584cbaefb6cf1d3494e80c58fba95e9a64a87b
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-10 20:50:46 -0500

    tech now broken up into categories

--- src/pages/Tech.tsx
+++ src/pages/Tech.tsx
@@ -6,6 +6,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  padding-top: 3.5rem;
 `;
 
 const TechTitle = styled.p<IProps>`
@@ -24,66 +25,90 @@ const TechContainer = styled.div`
 
 const CenterTech = styled.div`
   display: flex;
+  gap: 1rem;
   flex-wrap: wrap;
   justify-content: center;
-  max-width: 250px;
+  max-width: 700px;
   padding-top: 56px;
-  @media (min-width: 500px) {
-    max-width: 375px;
+`;
+
+const Stack = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+`;
+
+const TechItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+`;
+
+const Title = styled.p`
+  text-align: center;
+  font-size: 55px;
+  padding: 0;
+  margin: 0;
+  @media (max-width: 700px) {
+    font-size: 45px;
+  }
+  @media (max-width: 450px) {
+    font-size: 35px;
   }
 `;
 
 const TechItem = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 150px;
   font-size: 1rem;
   border: 1px solid black;
   padding: 1rem;
   border-radius: 15px;
-  margin-bottom: 1rem;
   @media (min-width: 500px) {
     font-size: 2rem;
   }
 `;
 
+// const Dash = styled.div`
+//   padding: 0 0 0 1rem;
+// `;
+
 const Tech = ({ height }: IProps) => {
   const technologies = [
-    'REACT',
-    'NEXT',
-    'EXPRESS',
-    'NODE',
-    'GRAPHQL',
-    'REST API',
-    'MYSQL',
-    'MONGODB',
+    { title: 'FRONT END', tech: ['REACT', 'NEXT JS'] },
+    { title: 'BACK END', tech: ['NODE', 'EXPRESS'] },
+    { title: 'API', tech: ['GRAPHQL', 'REST API'] },
+    { title: 'DATABASE', tech: ['MYSQL', 'MONGODB', 'AIRTABLE'] },
   ];
+
   return (
-    <Container>
+    <Container id='technology'>
       <TechTitle height={height}>TECHNOLOGIES</TechTitle>
       <TechContainer>
-        <Animate className='hidden'>
-          <CenterTech>
-            {technologies.map((tech, idx) => {
-              if (idx % 2 === 0) {
-                return (
-                  <div
-                    style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <TechItem>{tech}</TechItem>
-                    <div
-                      style={{ paddingLeft: '0.5rem', paddingRight: '0.5rem' }}
-                    >
-                      -
-                    </div>
-                  </div>
-                );
-              }
-              return <TechItem>{tech}</TechItem>;
-            })}
-          </CenterTech>
-        </Animate>
+        {/* <Animate className='hidden'> */}
+        <CenterTech>
+          {technologies.map(({ title, tech }) => {
+            return (
+              <Animate className='hidden'>
+                <Stack>
+                  <Title>{title}</Title>
+                  <TechItemContainer>
+                    {tech.map(item => {
+                      return <TechItem>{item}</TechItem>;
+                    })}
+                  </TechItemContainer>
+                </Stack>
+              </Animate>
+            );
+          })}
+        </CenterTech>
+        {/* </Animate> */}
       </TechContainer>
     </Container>
   );
