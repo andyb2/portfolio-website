93d6dd6649e77817341679118a874639c8de9802
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-10 19:05:49 -0500

    socials styling changed to be side by side. Socials are now rendered in the Nav component

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -2,20 +2,30 @@ import { Icon } from '@mui/material';
 import { Link } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import styled from '@emotion/styled';
 
 const style = {
-  color: '#c3ca86',
+  color: 'black',
   fontSize: '35px',
   marginLeft: '0.1rem',
 };
 
+const SocialsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.1rem;
+`;
+
+const iconCenter = { display: 'flex', justifyContent: 'center' };
+
 const Socials = () => {
   return (
-    <>
+    <SocialsContainer>
       <Link
         href='https://github.com/andyb2'
         aria-label='Github icon'
         target='_blank'
+        style={iconCenter}
       >
         <Icon sx={style} component={GitHubIcon} />
       </Link>
@@ -23,10 +33,14 @@ const Socials = () => {
         href='https://www.linkedin.com/in/andrew-boyle-a9a596204/'
         aria-label='LinkedIn Icon'
         target='_blank'
+        style={iconCenter}
       >
-        <Icon sx={style} component={LinkedInIcon} />
+        <Icon
+          sx={{ color: 'black', fontSize: '41.76px', marginLeft: '0.1rem' }}
+          component={LinkedInIcon}
+        />
       </Link>
-    </>
+    </SocialsContainer>
   );
 };
 
