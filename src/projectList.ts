6831504eb16cc71c8be46f1940e99a847ba27483
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-15 15:49:02 -0400

    fixed assistant captain description... tic tac toes description was in the wrong place

--- src/projectList.ts
+++ src/projectList.ts
@@ -2,14 +2,14 @@ export const projectsList = [
   {
     title: 'ASSISTANT CAPTAIN - REPO',
     description:
-      'Classic game where users choose tiles to connect three spaces in a row, column or diagonally. The user is playing against a bot that chooses their move depending on what the human player has selected',
+      'This project is a team collaboration and is ongoing. Assistant Captain is sports team roster management tool. Users controlling their teams can mass send texts to their players to confirm upcoming game attendances.',
     tech: ['NEXT.JS', 'FIREBASE', 'GRAPHQL', 'AIRBASE'],
     link: 'https://github.com/orrymevorach/assistant-captain',
   },
   {
     title: 'SPOTIFY USER - REPO',
     description:
-      'Spotify users can log into the application and display their recently played songs, top songs, top artists, and playlists. ',
+      'This project is not live because the Spotify API can no longer be used for hobby projects. Spotify users can log into the application and display their recently played songs, top songs, top artists, and playlists. ',
     tech: ['REACT', 'REDUX', 'EXPRESS', 'NODE', 'SPOTIFY API'],
     link: 'https://github.com/andyb2/Spotify-User-Profile',
     image: './img/SpotifyProfile.JPG',
@@ -24,7 +24,7 @@ export const projectsList = [
   {
     title: 'TIC TAC TOE - LIVE APP',
     description:
-      'Classic game where users choose tiles to connect three spaces in a row, column or diagonally. The user is playing against a bot that chooses their move depending on what the human player has selected',
+      'Classic game where users choose tiles to connect three spaces in a row, column or diagonally. The user is playing against a bot that chooses their move depending on what the human player has selected.',
     tech: ['TYPESCRIPT', 'REACT', 'REDUX'],
     link: 'https://andyb2.github.io/tictactoe/',
   },
